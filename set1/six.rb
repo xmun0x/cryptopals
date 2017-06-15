1ae941746777e24c3222c820b44f17e17804711c
Cristina Munoz <hi@xmunoz.com>
2017-06-14 21:54:56 -0700

    make code more ruby-esque

--- set1/six.rb
+++ set1/six.rb
@@ -15,46 +15,47 @@ def ascii2bin(a)
     result
 end
 
-def hammingDistance(a, b)
+def hamming_distance(a, b)
     a = ascii2bin(a)
     b = ascii2bin(b)
     fail "hammingDistance: unequal input lengths" if a.length != b.length
     (a.chars.zip(b.chars)).count {|l, r| l != r}
 end
 
-def breakRepeatingKeyXOR(filename)
+def break_repeating_key_xor(filename)
     lines = ""
     file = File.new(filename, "r")
     while (line = file.gets)
         lines += line.chomp
     end
+
     lines = base642hex(lines)
     smallest_keysize = 2
     largest_keysize = 40
     distances = []
     n = 8 # re-run a few times while changing this number to be sure of top result
 
-    for i in (smallest_keysize*2..largest_keysize*2).step(2)
-        a = lines.slice(0, i*n)
-        b = lines.slice(i*n, i*n)
-        c = lines.slice(2*i*n, i*n)
-        normdist1 = hammingDistance(a, b)/(Float(i) * 8)
-        normdist2 = hammingDistance(b, c)/(Float(i) * 8)
-        normdist3 = hammingDistance(a, c)/(Float(i) * 8)
-        average = (normdist1 + normdist2 + normdist3)/3
-        distances.push({"keysize" => i/2, "distance" => average})
+    (smallest_keysize * 2..largest_keysize * 2).step(2) do |i|
+        a = lines.slice(0, i * n)
+        b = lines.slice(i * n, i * n)
+        c = lines.slice(2 * i * n, i * n)
+        normdist1 = hamming_distance(a, b) / (Float(i) * 8)
+        normdist2 = hamming_distance(b, c) / (Float(i) * 8)
+        normdist3 = hamming_distance(a, c) / (Float(i) * 8)
+        average = (normdist1 + normdist2 + normdist3) / 3
+        distances.push({"keysize" => i / 2, "distance" => average})
     end
 
     sorted = distances.sort_by { |k| k["distance"] }
     keysize = sorted[0]['keysize']
 
     blocks = {}
-    for i in 0..keysize - 1
+    (0..keysize - 1).each do |i|
         blocks[i] = ""
     end
 
-    for i in 0..(lines.length/2) - 1
-        blocks[i%keysize] += lines.slice(i*2, 2)
+    (0..(lines.length / 2) - 1).each do |i|
+        blocks[i % keysize] += lines.slice(i * 2, 2)
     end
 
     key = ""
@@ -66,7 +67,7 @@ def breakRepeatingKeyXOR(filename)
     puts "The key is '#{key}'"
 
     full_key = ""
-    for i in 0..(lines.length/2) - 1
+    (0..(lines.length / 2) - 1).each do |i|
         full_key += key[i % key.length]
     end
 
@@ -81,8 +82,8 @@ end
 if __FILE__ == $0
     s1 = "this is a test"
     s2 = "wokka wokka!!!"
-    fail if !hammingDistance(s1, s2).eql?(37)
-    key = breakRepeatingKeyXOR("6.txt")
-    fail if !key.eql?("Terminator X: Bring the noise")
+    fail unless hamming_distance(s1, s2).eql?(37)
+    key = break_repeating_key_xor("6.txt")
+    fail unless key.eql?("Terminator X: Bring the noise")
     puts "Challenge #6 tests passed"
 end
